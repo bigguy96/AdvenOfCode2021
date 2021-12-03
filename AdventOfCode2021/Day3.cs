1001e9e1da28b6d2c3739edd8402d61ade0af7be
GT <26967905+bigguy96@users.noreply.github.com>
2021-12-03 13:34:30 -0500

    Day 3 - Part 1

---
+++ AdventOfCode2021/Day3.cs
@@ -0,0 +1,60 @@
+﻿namespace AdventOfCode2021;
+
+public class Day3 : Days
+{
+    private const string Filename = "day3-input.txt";
+    private static readonly string[] Input = GetFileContents(Filename);
+
+    public static int PartOne()
+    {
+        var gamma = string.Empty;
+        var epsilon = string.Empty;
+        var maxLength = Input[0].ToCharArray().Length;
+        var bits = new char[Input.Length, maxLength];
+
+        for (var i = 0; i < Input.Length; i++)
+        {
+            var bit = Input[i];
+
+            for (var j = 0; j < maxLength; j++)
+            {
+                bits[i, j] = bit[j];
+            }
+        }
+
+        for (var i = 0; i < maxLength; i++)
+        {
+            var zeroes = 0;
+            var ones = 0;
+
+            for (var j = 0; j < Input.Length; j++)
+            {
+                var value = bits[j, i];
+                if (value.Equals('0'))
+                {
+                    zeroes++;
+                }
+                else
+                {
+                    ones++;
+                }
+            }
+
+            if (ones > zeroes)
+            {
+                gamma += "1";
+                epsilon += "0";
+            }
+            else
+            {
+                epsilon += "1";
+                gamma += "0";
+            }
+        }
+
+        var gammaNumber = Convert.ToInt32(gamma, 2);
+        var epsilonNumber = Convert.ToInt32(epsilon,2);
+
+        return gammaNumber * epsilonNumber;
+    }
+}
--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -1,12 +1,11 @@
 ﻿using AdventOfCode2021;
 
-
-
-
 Console.WriteLine($"Day 1 - Part 1: {Day1.GetSum(1)}");
 Console.WriteLine($"Day 1 - Part 2: {Day1.GetSum(3)}");
 
 Console.WriteLine($"Day 2 - Part 1: {Day2.PartOne()}");
 Console.WriteLine($"Day 2 - Part 2: {Day2.PartTwo()}");
+
+Console.WriteLine($"Day 3 - Part 1: {Day3.PartOne()}");
 
 Console.ReadLine();
