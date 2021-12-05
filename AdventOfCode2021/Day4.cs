1bcd6ebf8e81b3bb39a9b4c6c84a79d2819cd793
GT <26967905+bigguy96@users.noreply.github.com>
2021-12-05 17:48:03 -0500

    Work in progres - Day 4 - Part 1.

---
+++ AdventOfCode2021/Day4.cs
@@ -0,0 +1,63 @@
+﻿namespace AdventOfCode2021;
+
+public class Day4 : Days
+{
+    private const string Filename = "day4-input.txt";
+    private static readonly string[] Input = GetFileContents(Filename);
+
+    public static int PartOne()
+    {
+        var numbers = Input[0].Split(',', StringSplitOptions.RemoveEmptyEntries).Select(number => Convert.ToInt32(number)).ToList();
+        var boardNumbers = Input.Where(w=> !string.IsNullOrEmpty(w)).Skip(1).ToList();
+        var boards = new List<Board>();
+        var currentIndex = 1;
+
+        foreach (var value in boardNumbers)
+        {
+            var board = new Board
+            {
+                Name = $"Board: {currentIndex}",
+                Rows = new List<Rows>
+                {
+                    new()
+                    {
+                        Numbers = value.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(s=>  Convert.ToInt32(s))
+                    }
+                }
+            };
+
+            boards.Add(board);
+
+            if (boards.Count %5 == 0) currentIndex++;
+        }
+
+        var groupedBoards = boards.GroupBy(x => x.Name);
+
+        foreach (var group in groupedBoards)
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine(group.Key);
+
+            foreach (var board in group)
+            {
+                foreach (var row in board.Rows)
+                {
+                    Console.WriteLine(string.Join(" " , row.Numbers));
+                }
+            }
+        }
+
+        return 0;
+    }
+}
+
+public class Board
+{
+    public string Name { get; set; }
+    public IEnumerable<Rows> Rows { get; set; }
+}
+
+public class Rows
+{
+    public IEnumerable<int> Numbers { get; set; }
+}
--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -9,4 +9,6 @@ Console.WriteLine($"Day 2 - Part 2: {Day2.PartTwo()}");
 Console.WriteLine($"Day 3 - Part 1: {Day3.PartOne()}");
 Console.WriteLine($"Day 3 - Part 2: {Day3.PartTwo()}");
 
+Console.WriteLine($"Day 4 - Part 1: {Day4.PartOne()}");
+
 Console.ReadLine();
