38bb1311e06997f19438b39d081bb736d78e49ff
GT <26967905+bigguy96@users.noreply.github.com>
2021-12-08 13:19:40 -0500

    Added Day 5 files.

---
+++ AdventOfCode2021/Day5/Day5.cs
@@ -0,0 +1,20 @@
+﻿namespace AdventOfCode2021.Day5;
+
+public class Day5 : Days
+{
+    private const string Filename = "day5-input.txt";
+    private static readonly string[] Input = GetFileContents(Filename);
+
+    public static string PartOne()
+    {
+
+        return "";
+    }
+
+    public static string PartTwo()
+    {
+
+
+        return "";
+    }
+}
--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -2,6 +2,7 @@
 using AdventOfCode2021.Day2;
 using AdventOfCode2021.Day3;
 using AdventOfCode2021.Day4;
+using AdventOfCode2021.Day5;
 
 Console.WriteLine($"Day 1 - Part 1: {Day1.GetSum(1)}");
 Console.WriteLine($"Day 1 - Part 2: {Day1.GetSum(3)}");
@@ -14,5 +15,7 @@ Console.WriteLine($"Day 3 - Part 2: {Day3.PartTwo()}");
 
 Console.WriteLine($"Day 4 - Part 1: {Day4.PartOne()}");
 Console.WriteLine($"Day 4 - Part 2: {Day4.PartTwo()}");
+
+Console.WriteLine($"Day 5 - Part 1: {Day5.PartOne()}");
 
 Console.ReadLine();
