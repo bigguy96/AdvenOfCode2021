c3b44e58295117ac49eaa5997b0c4de8be1ca93b
GT <26967905+bigguy96@users.noreply.github.com>
2021-12-08 13:52:22 -0500

    Day 6 - Part 1 - Solution

---
+++ AdventOfCode2021/Day6/Day6.cs
@@ -0,0 +1,41 @@
+﻿namespace AdventOfCode2021.Day6;
+
+public class Day6 : Days
+{
+    private const string Filename = "day6-input.txt";
+    private static readonly string[] Input = GetFileContents(Filename);
+
+    public static string PartOne()
+    {
+        var fishes = Input[0]
+            .Split(',', StringSplitOptions.RemoveEmptyEntries)
+            .Select(number => Convert.ToInt32(number))
+            .ToList();
+
+        for (var i = 0; i < 18; i++)
+        {
+            var count = fishes.Count;
+            for (var j = 0; j < count; j++)
+            {
+                if (fishes[j]== 0)
+                {
+                    fishes[j] = 6;
+                    fishes.Add(8);
+                }
+                else
+                {
+                    fishes[j]--;
+                }
+            }
+        }
+
+        return fishes.Count.ToString();
+    }
+
+    public static string PartTwo()
+    {
+
+
+        return "";
+    }
+}
--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -3,6 +3,7 @@ using AdventOfCode2021.Day2;
 using AdventOfCode2021.Day3;
 using AdventOfCode2021.Day4;
 using AdventOfCode2021.Day5;
+using AdventOfCode2021.Day6;
 
 Console.WriteLine($"Day 1 - Part 1: {Day1.GetSum(1)}");
 Console.WriteLine($"Day 1 - Part 2: {Day1.GetSum(3)}");
@@ -17,5 +18,8 @@ Console.WriteLine($"Day 4 - Part 1: {Day4.PartOne()}");
 Console.WriteLine($"Day 4 - Part 2: {Day4.PartTwo()}");
 
 Console.WriteLine($"Day 5 - Part 1: {Day5.PartOne()}");
+
+Console.WriteLine($"Day 6 - Part 1: {Day6.PartOne()}");
+Console.WriteLine($"Day 6 - Part 2: {Day6.PartTwo()}");
 
 Console.ReadLine();
